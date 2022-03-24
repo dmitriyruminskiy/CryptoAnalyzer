f779fc6d8f934a2c7ace9205c342063320723a48
Dima Rumynskii <ra-z88@mail.ru>
2022-03-24 08:44:11 +0300

    test commit

--- src/main/java/ru/javarush/rumynskii/cryptoanalizer/commands/Encoder.java
+++ src/main/java/ru/javarush/rumynskii/cryptoanalizer/commands/Encoder.java
@@ -1,12 +1,46 @@
 package ru.javarush.rumynskii.cryptoanalizer.commands;
 
+import ru.javarush.rumynskii.cryptoanalizer.constants.Constants;
 import ru.javarush.rumynskii.cryptoanalizer.entity.Result;
 import ru.javarush.rumynskii.cryptoanalizer.entity.ResultCode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Encoder implements Action{
 
     @Override
     public Result execute(String[] parameters) {
+        try
+                (FileReader reader = new FileReader(parameters[0]);
+                 FileWriter writer = new FileWriter(parameters[1])) {
+            char[] buffer = new char[65536];
+            while (reader.ready()) {
+                int real = reader.read(buffer);
+                char[] bufferNew = encodeBuffer(buffer,parameters[2]);
+                writer.write(bufferNew, 0, real);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return new Result("encode all right", ResultCode.OK);
     }
+    public char[] encodeBuffer(char[] buffer, String keyInString) {
+        int key = Integer.parseInt(keyInString);
+        char[] alphabet = Constants.ALPHABET.toCharArray();
+        char[] bufferEncode = new char[65536];
+        for (int i = 0; i < buffer.length; i++) {
+            for (int j = 0; j < alphabet.length; j++) {
+                if (buffer[i] == alphabet[j]) {
+                    bufferEncode[i] = alphabet[(j + key) % 147];
+                    break;
+                }
+            }
+        }
+        return bufferEncode;
+    }
+
 }
--- src/main/java/ru/javarush/rumynskii/cryptoanalizer/constants/Constants.java
+++ src/main/java/ru/javarush/rumynskii/cryptoanalizer/constants/Constants.java
@@ -1,10 +1,10 @@
 package ru.javarush.rumynskii.cryptoanalizer.constants;
 
 public class Constants {
-    private static final String rus = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ";
-    private static final String eng = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private static final String rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЪЭЮЯ";
+    private static final String eng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String cypher = "0123456789";
-    private static final String z = "!@#$%^&*()[]{}";
-    public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + z;
+    private static final String z = "!@#$%^&*()[]{}?.,-";
+    public static final String ALPHABET = rus + eng + rus.toLowerCase() + eng.toLowerCase() + cypher + z + ' ';
 
 }
